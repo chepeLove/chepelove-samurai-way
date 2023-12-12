7e7aad35a8c1f9de900bec723161fa22ac8b7997
chepeLove <ilya.chepelov@gmail.com>
2023-12-12 11:23:17 +0300

    feat: add REDUX_DEVTOOLS_EXTENSION_COMPOSE in store

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,11 +1,11 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import {ProfileActionsType, ProfileReducer} from "./profile-reducer";
 import {DialogsActionsType, DialogsReducer} from "./dialogs-reducer";
 import {UsersActionsType, UsersReducer} from "./users-reducer";
 import {AuthActionsType, AuthReducer} from "./auth-reducer";
 import {ThunkAction} from "redux-thunk";
 import thunkMiddleware from "redux-thunk";
-import {reducer as formReducer} from 'redux-form'
+import { reducer as formReducer} from 'redux-form'
 import {AppReducer} from "./app-reducer";
 
 const rootReducer = combineReducers({
@@ -21,7 +21,12 @@ const rootReducer = combineReducers({
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer,applyMiddleware(thunkMiddleware));
+
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+// export const store = createStore(rootReducer,applyMiddleware(thunkMiddleware));
 
 export type AppActionsType = UsersActionsType | ProfileActionsType | DialogsActionsType | AuthActionsType
 
